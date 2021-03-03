3054754a5809c85430a88d41a5b29bf651a58dd2
Alan Lee <alleetw101@gmail.com>
2021-03-03 16:46:37 -0500

    Updated load_dataset for more options, updated resize and image augmentation with options for image and improved slice augmentation

--- models/loaddataset.py
+++ models/loaddataset.py
@@ -5,65 +5,66 @@ import numpy as np
 import os
 
 
-def load_structure_seg_data():
-    dataset_path = 'dataverse_files/NKI-RS-22_volumes/NKI-RS-22-2/t1weighted.nii.gz'
-    data_image = sitk.ReadImage(dataset_path, sitk.sitkFloat32)
-    x = sitk.GetArrayFromImage(data_image)
-
-    # x = np.where(1000.0 <= x < 2000.0, x, x - 1000.0)
-    x = np.where(2000.0 > x, x, x - 2000.0)
-    x = np.where(1000.0 > x, x, x - 1000.0)
-
-    plt.figure(figsize=(9, 9))
-
-    for position in range(4 * 4):
-        plt.subplot(4, 4, position + 1)
-        plt.xticks([])
-        plt.yticks([])
-        plt.imshow(x[50 + (position * 10)])
-        plt.title(f'Slice index: {50 + (position * 10)}')
-    plt.show()
-
-    print(x.shape)
-
-
-def load_whole_brain_seg_data(): # , ds_list: [str, ], num: int = None):
+def load_whole_brain_seg_data(set_start: int, set_end: bool, structures: bool, base_raw: bool,
+                              mni152: bool, structures_cortex: bool,
+                              resize: bool, resize_shape: [int, int, int],
+                              augment: bool, augment_only_images: bool):
     datadirpath = 'dataverse_files'
     datasetpath_dic = {
         'NKI-RS': 'NKI-RS-22_volumes',
         'OASIS-TRT': 'OASIS-TRT-20_volumes',
         'MMRR': 'MMRR-21_volumes',
-        'NKI-TRT': 'NKI-TRT-20_volumes'
+        'NKI-TRT': 'NKI-TRT-20_volumes',
+        'Extra': 'Extra-18_volumes'
     }
 
-    # if ds_list[0].lower() == 'all':
-    dataset_list = list(datasetpath_dic.values())
-    # else:
-    #     dataset_list = list(map(lambda x: datasetpath_dic[x], ds_list))
-    print(dataset_list)
+    if set_start == 0 and set_end == 0:
+        dataset_list = list(datasetpath_dic.values())
+    else:
+        dataset_list = list(datasetpath_dic.values())[set_start:set_end]
+
+    scan_file_name = 't1weighted'
+    if not base_raw:
+        scan_file_name = scan_file_name + '_brain'
+
+    if structures:
+        mask_file_name = 'labels.DKT31.manual+aseg'
+    else:
+        mask_file_name = 't1weighted_brain'
+
+    if mni152:
+        scan_file_name = scan_file_name + '.MNI152'
+        mask_file_name = mask_file_name + '.MNI152'
+
+    scan_file_name = scan_file_name + '.nii.gz'
+    mask_file_name = mask_file_name + '.nii.gz'
 
     for dataset in dataset_list:
         dataset_path = os.path.join(datadirpath, dataset)
-        dir_list = [x for x in os.listdir(dataset_path) if dataset[:4] in x]
-
-        # if num:
-        #     dir_list.sort()
-        #     dir_list = dir_list[:num]
+        dir_list = [x for x in os.listdir(dataset_path) if '-' in x]
 
         for directory in dir_list:
-            raw_scan_file = os.path.join(dataset_path, directory, 't1weighted.MNI152.nii.gz')
-            brain_scan_file = os.path.join(dataset_path, directory, 't1weighted_brain.MNI152.nii.gz')
+            raw_scan_file = os.path.join(dataset_path, directory, scan_file_name)
+            brain_scan_file = os.path.join(dataset_path, directory, mask_file_name)
 
             scan_image = sitk.GetArrayFromImage(sitk.ReadImage(raw_scan_file, sitk.sitkFloat32))
             scan_image /= np.amax(scan_image)
 
             mask_image = sitk.GetArrayFromImage(sitk.ReadImage(brain_scan_file, sitk.sitkFloat32))
-            mask_image = np.where(0 == mask_image, mask_image, 1)
 
-            # scan_image = resize_256(scan_image)
-            # mask_image = resize_256(mask_image)
+            if not structures:
+                mask_image = np.where(0 == mask_image, mask_image, 1)
 
-            # scan_image, mask_image = image_augmentation(scan_image, mask_image)
+            if structures and not structures_cortex:
+                mask_image = np.where(2000.0 > mask_image, mask_image, mask_image - 1000.0)
+                mask_image = np.where(1000.0 > mask_image, mask_image, mask_image - 1000.0)
+
+            if resize:
+                scan_image = resize_256(scan_image, resize_shape)
+                mask_image = resize_256(mask_image, resize_shape)
+
+            if augment:
+                scan_image, mask_image = image_augmentation(scan_image, mask_image, augment_only_images)
 
             scan_image = np.expand_dims(scan_image, axis=-1)
             mask_image = np.expand_dims(mask_image, axis=-1)
@@ -72,38 +73,11 @@ def load_whole_brain_seg_data(): # , ds_list: [str, ], num: int = None):
                 yield scan_image[slices], mask_image[slices]
 
 
-def load_whole_brain_seg_extra(datadirpath, num: int = None):
-    # TF dataset from Extra-18_volumes subjects for evaluation
-
-    dataset_path = os.path.join(datadirpath, 'Extra-18_volumes')
-    dir_list = [x for x in os.listdir(dataset_path) if '-' in x]
-
-    if num:
-        dir_list.sort()
-        dir_list = dir_list[:num]
-
-    for directory in dir_list:
-        raw_scan_file = os.path.join(dataset_path, directory, 't1weighted.nii.gz')
-        brain_scan_file = os.path.join(dataset_path, directory, 't1weighted_brain.nii.gz')
-
-        scan_image = sitk.GetArrayFromImage(sitk.ReadImage(raw_scan_file, sitk.sitkFloat32))
-        scan_image /= np.amax(scan_image)
-
-        mask_image = sitk.GetArrayFromImage(sitk.ReadImage(brain_scan_file, sitk.sitkFloat32))
-        mask_image = np.where(0 == mask_image, mask_image, 1)
-
-        scan_image = resize_256(scan_image)
-        mask_image = resize_256(mask_image)
-
-        scan_image = np.expand_dims(scan_image, axis=-1)
-        mask_image = np.expand_dims(mask_image, axis=-1)
-
-        for slices in range(len(scan_image)):
-            yield scan_image[slices], mask_image[slices]
-
-
-def resize_256(array) -> np.array:
-    shape_dif = np.subtract([256, 256, 256], array.shape)
+def resize_256(array, resize_shape: [int, int, int]) -> np.array:
+    for dim in range(len(resize_shape)):
+        if resize_shape[dim] == 0:
+            resize_shape[dim] = array.shape[dim]
+    shape_dif = np.subtract(resize_shape, array.shape)
 
     # Padding
     pad_list = []
@@ -136,62 +110,41 @@ def resize_256(array) -> np.array:
     return output_array
 
 
-def image_augmentation(scan_image, mask_image):
-    tranpose_array = [0, 1, 2]
+def image_augmentation(scan_image, mask_image, augment_only_images=True):
     rng = np.random.default_rng()
-    rng.shuffle(tranpose_array)
 
-    scan = np.transpose(scan_image, tranpose_array)
-    mask = np.transpose(mask_image, tranpose_array)
+    if augment_only_images:
+        scan = scan_image
+        mask = mask_image
+        for index in range(len(scan_image)):
+            if bool(rng.integers(2, size=1)[0]):
+                scan[index] = np.transpose(scan[index], [1, 0])
+                mask[index] = np.transpose(mask[index], [1, 0])
+    else:
+        tranpose_array = [0, 1, 2]
+        rng.shuffle(tranpose_array)
 
-    if bool(rng.integers(1, size=1)[0]):
-        scan = np.flip(scan, axis=1)
-        mask = np.flip(mask, axis=1)
+        scan = np.transpose(scan_image, tranpose_array)
+        mask = np.transpose(mask_image, tranpose_array)
 
-    if bool(rng.integers(1, size=1)[0]):
-        scan = np.flip(scan, axis=2)
-        mask = np.flip(mask, axis=2)
+    for index in range(len(scan_image)):
+        if bool(rng.integers(2, size=1)[0]):
+            scan[index] = np.flip(scan[index], axis=0)
+            mask[index] = np.flip(mask[index], axis=0)
+
+        if bool(rng.integers(2, size=1)[0]):
+            scan[index] = np.flip(scan[index], axis=1)
+            mask[index] = np.flip(mask[index], axis=1)
 
     return scan, mask
 
 
-def testing():
-    dataset_path = 'dataverse_files/MMRR-21_volumes/MMRR-21-1/t1weighted.MNI152.nii.gz'
-    data_image = sitk.ReadImage(dataset_path, sitk.sitkFloat32)
-    x = sitk.GetArrayFromImage(data_image)
-
-    # x = np.where(0 == x, x, 1)
-    # x = np.transpose(x, [2, 1, 0])
-    # x = np.flip(x, axis=1)
-    # x = np.pad(x, [(0, 0), (0, 0), (16, 16)], mode='constant', constant_values=0.0)
-    # x = resize_256(x)
-    # x, _ = image_augmentation(x, x)
-
-    # x = np.transpose(x, [2,0,1])
-
-    plt.figure(figsize=(9, 9))
-
-    if True:
-        for position in range(3 * 4):
-            plt.subplot(3, 4, position + 1)
-            plt.xticks([])
-            plt.yticks([])
-            plt.imshow(x[20 + (position * 10)], cmap='gray')
-            plt.title(f'Slice index: {20 + (position * 10)}')
-    else:
-        plt.imshow(x[100])
-
-    plt.show()
-    print(x.shape)
-
-
-def display_test(dataset, samples: int = 5, start: int = 500):
+def display_test(dataset, samples: int = 5, start: int = 120):
     plt.figure(figsize=(9, 9))
     images = []
-    for index in range(samples):
-        for scan, mask in dataset.skip(start + index).take(1):
-            images.append(np.squeeze(scan.numpy(), axis=-1))
-            images.append(np.squeeze(mask.numpy(), axis=-1))
+    for scan, mask in dataset.take(samples):
+        images.append(np.squeeze(scan[0], axis=-1))
+        images.append(np.squeeze(mask[0], axis=-1))
 
     for i in range(samples * 2):
         plt.subplot(samples, 2, i + 1)
@@ -200,28 +153,12 @@ def display_test(dataset, samples: int = 5, start: int = 500):
     plt.show()
 
 
-# testing()
-
-dir_path = 'dataverse_files'
-ds_gen = load_whole_brain_seg_data() # , ds_list=['all'])
-# ds_gen = load_whole_brain_seg_extra(dir_path)
-gen = lambda: (pair for pair in ds_gen)
-# ds = load_whole_brain_seg_extra(dir_path).shuffle(2000)
-#
-ds = tf.data.Dataset.from_generator(load_whole_brain_seg_data, output_signature=(
-    tf.TensorSpec(shape=(218, 182, 1), dtype=tf.float32),
-    tf.TensorSpec(shape=(218, 182, 1), dtype=tf.float32)))
-
-counter = 0
-for test, maks in ds:
-    print(counter)
-    counter += 1
-
-# display_test(ds, samples=10)
-# print(ds.cardinality())
-
-# image_augmentation([], [])r
-
-print('end')
-# test_array = np.zeros((20, 25, 4005))
-# resize_256(test_array)
+# Sample Dataset from Generator
+# args = [set_start, set_end, structures, base_raw, mni152, structures_cortex, resize, resize_shape, augment, augment_only_images]
+# ds = tf.data.Dataset.from_generator(load_whole_brain_seg_data,
+#                                     args=[4, 5, False, True, True, False, True, [0, 256, 256], True, True],
+#                                     output_signature=(
+#                                         tf.TensorSpec(shape=(256, 256, 1), dtype=tf.float32),
+#                                         tf.TensorSpec(shape=(256, 256, 1), dtype=tf.float32)))
+# ds = ds.batch(16)
+# display_test(ds)
